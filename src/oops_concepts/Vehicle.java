03ee7e3963fc5ed37f7f920aace1540cca5a6bc3
rohan2734 <jagandevaki1@gmail.com>
2020-11-13 19:38:00 +0530

    added time complexity questions answers

--- src/oops_concepts/Vehicle.java
+++ src/oops_concepts/Vehicle.java
@@ -10,17 +10,7 @@ public class Vehicle {
 		System.out.println("Vehicles constructor");
 	}
 	
-	/**
-	 * here the maxSpeed is private, only Vehicle class can access it,
-	 * even the children of the Vehicle class cannot access the maxSpeed
-	 */
 	
-	/**
-	 * protected
-	 * is friendly to the package and
-	 * it can be accessed outside the package  only to the children class
-	 * @return
-	 */
 	public int getMaxSpeed() {
 		return maxSpeed;
 	}
