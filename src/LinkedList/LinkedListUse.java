06310508b0f39845800726c45137e231ee9abbbc
rohan2734 <jagandevaki1@gmail.com>
2020-11-16 10:42:56 +0530

    added linkedListUse

--- src/LinkedList/LinkedListUse.java
+++ src/LinkedList/LinkedListUse.java
@@ -19,15 +19,7 @@ public class LinkedListUse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Node<Integer> node1= new Node<Integer>(10);
-//		System.out.println(node1.data);
-//		System.out.println(node1.next);
-//		
-//		Node<Integer> node2= new Node<Integer>(20);
-//		node1.next = node2;
-//		
-//		System.out.println(node1.next);
-//		System.out.println(node2);
+
 		
 		Node<Integer> node1= new Node<Integer>(10);
 		Node<Integer> node2= new Node<Integer>(20);
---
+++ src/LinkedList/takeInput_LinkedListUse.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class takeInput_LinkedListUse {
+	
+	public static Node<Integer> takeInput(){
+		Node<Integer> head = null;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
