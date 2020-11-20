3be0c8c02f679f0fa09c97daeadeb3b7f2cff8e4
rohan2734 <jagandevaki1@gmail.com>
2020-11-19 20:55:55 -0800

    added eliminate duplicates, and priting reverse in linedlist

---
+++ src/LinkedList/eliminate_duplicates.java
@@ -0,0 +1,75 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class eliminate_duplicates {
+
+	/**
+	 * @param args
+	 */
+	public static Node<Integer> takeInput(){
+		Node<Integer> head = null,tail = null;
+		Scanner s = new Scanner(System.in);
+		int data = s.nextInt();
+		
+		while(data != -1){
+			Node<Integer> newNode = new Node<Integer>(data);
+			if(head == null){
+				head = newNode;
+				tail = newNode;
+			}else{
+				Node<Integer> temp = head;
+				tail.next = newNode;
+				tail = newNode;
+			}
+			data=s.nextInt();
+		}
+		return head;
+	}
+	public static void printNodes(Node<Integer> head){
+		Node<Integer> temp = head;
+		
+		while(head != null) {
+			System.out.print(head.data +" ");
+			head = head.next;
+		}
+		System.out.println();
+		head=temp;
+	}
+	
+	public static Node<Integer> eliminateDuplicates(Node<Integer> head){
+		Node<Integer> temp1 = head;
+		Node<Integer> temp2 = head;
+        if(temp1.next == null){
+            return head;
+        }
+        temp2=temp2.next;
+		
+		while(temp1!=null ){
+			
+            
+			while(temp2 != null && temp1.data == temp2.data){
+				temp2=temp2.next;
+			}
+			temp1.next =temp2;
+            temp1=temp2;
+            if(temp2!=null){
+            	 temp2=temp2.next;
+            }
+           
+		}
+       
+		return head;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node<Integer> head = takeInput();
+		
+		head = eliminateDuplicates(head);
+		printNodes(head);
+		
+		
+		
+	}
+
+}
---
+++ src/LinkedList/print_reverse_linked_list.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class print_reverse_linked_list {
+	
+	public static Node<Integer> takeInput(){
+		Node<Integer> head = null,tail = null;
+		Scanner s = new Scanner(System.in);
+		int data = s.nextInt();
+		
+		while(data != -1){
+			Node<Integer> newNode = new Node<Integer>(data);
+			if(head == null){
+				head = newNode;
+				tail = newNode;
+			}else{
+				Node<Integer> temp = head;
+				tail.next = newNode;
+				tail = newNode;
+			}
+			data=s.nextInt();
+		}
+		return head;
+	}
+	
+
+	public static void printReverse(Node<Integer> head) {
+		//Your code goes here
+		Node<Integer> temp = head;
+		Node<Integer> tail = head;
+		
+        if(head == null){
+            return;
+        }
+		while(temp.next !=null){
+			temp=temp.next;
+		}
+		tail= temp;
+        
+		
+		while(head != tail){
+			temp=head;
+			while(temp.next !=tail){
+				temp=temp.next;
+			}
+			System.out.print(tail.data+" ");
+			tail=temp;
+		}
+		System.out.print(head.data +" ");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Node<Integer> head = takeInput();
+		printReverse(head);
+	}
+
+}
