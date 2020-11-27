e62f767d0bb74e4a2c75141b6dcb794caf510fde
rohan2734 <jagandevaki1@gmail.com>
2020-11-26 22:40:26 -0800

    added swap_nodes

--- src/LinkedList_2/swap_two_nodes.java
+++ src/LinkedList_2/swap_two_nodes.java
@@ -33,6 +33,48 @@ public class swap_two_nodes {
 		System.out.println();
 		head=temp;
 	}
+	
+	public static Node<Integer> lecturers_swapNodes(Node<Integer> head,int i,int j){
+		if(i == j){
+			return head;
+		}
+		
+		Node<Integer> currentNode = head,prev = null;
+		Node<Integer> firstNode = null,secondNode = null,firstNodePrev = null,secondNodePrev = null;
+		
+		int pos = 0;
+		while(currentNode !=null){
+			if(pos == i){
+				firstNodePrev = prev;
+				firstNode = currentNode;
+			}else if(pos == j){
+				secondNodePrev = prev;
+				secondNode = currentNode;
+			}
+			
+			prev = currentNode;
+			currentNode= currentNode.next;
+			pos += 1;
+		}
+		
+		if(firstNodePrev != null){
+			firstNodePrev.next = secondNode;
+		}else{
+			head = secondNode;
+		}
+		
+		if(secondNodePrev != null){
+			secondNodePrev.next = firstNode;
+		}else{
+			head = firstNode;
+		}
+		
+		Node<Integer> currentFirstNode = secondNode.next;
+		secondNode.next = firstNode.next;
+		firstNode.next = currentFirstNode;
+		
+		return head;
+	}
 	public static Node<Integer> swapNodes(Node<Integer> head,int m,int n){
 		Node<Integer> temp = head;
 		Node<Integer> prevN= head;
